e93c4512ebb9e820c8c8486e49779de86046dec5
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-04-08 18:45:51 -0300

    archive for interfaces

---
+++ src/types/types.ts
@@ -0,0 +1,27 @@
+export interface MovieProps {
+   id?: number;
+   title: string;
+   poster_path: string;
+   release_date: string;
+   vote_average: number;
+   overview: string
+   runtime?: number;
+   isInWatchList: boolean;
+   addToWatchList: () => void;
+   removeFromWatchList: () => void;
+}
+
+export interface ButtonProps {
+   emoji: React.ReactNode;
+   text: string;
+   active: boolean;
+   onClick: () => void;
+}
+
+export interface SideBarProps {
+   watchList: number;
+   renderWatchList: () => void;
+   onPopularClick: () => void;
+   onTopRatedClick: () => void;
+   onUpCommingClick: () => void;
+}
