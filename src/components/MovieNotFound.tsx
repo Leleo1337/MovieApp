96f302f4912fc6469141604fcd4d8b8e42ec1bce
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-04-09 17:42:56 -0300

    refactored movie not found component

--- src/components/MovieNotFound.tsx
+++ src/components/MovieNotFound.tsx
@@ -1,13 +1,8 @@
 import { Film } from "lucide-react";
 
-type MovieNotFoundProps = {
-   searchQuery: string;
-};
-
-export default function MovieNotFound({ searchQuery }: MovieNotFoundProps) {
+export default function MovieNotFound() {
    return (
       <div className="w-full">
-         <p className="text-xl text-gray-300 font-semibold">Search results for "{searchQuery}"</p>
          <div className="flex flex-col justify-center items-center w-full rounded-md bg-secondary/80 mx-auto h-80">
             <Film size={60} color="gray" />
             <h1 className="text-xl font-bold text-gray-400">No movies found</h1>
