71800ab7dc9cee4db99667217f629a00e39e0361
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-04-10 12:22:22 -0300

    testing vercel routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Main from './pages/Main'
 
 export default function App(){
     return(
         <>
-            <BrowserRouter basename='/MovieWatchList/'>
-                <Routes>
-                    <Route path="/" element={<Login />} />
-                    <Route path="/Main" element={<Main />} />
-                </Routes>
-            </BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/Main" element={<Main />} />
+            </Routes>
         </>
     )
 }
