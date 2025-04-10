044b9ff505b3e8997490dad7a769350d8de7e3c5
Leandro <168025120+Leleo1337@users.noreply.github.com>
2025-04-10 12:35:10 -0300

    fixed width

--- src/components/SearchDropDown.tsx
+++ src/components/SearchDropDown.tsx
@@ -5,7 +5,7 @@ export default function SearchResult({title, poster_path, overView, click}: Sear
         <li onClick={click} className="bg-secondary border-t border-gray-200/20 p-1.5 hover:bg-button">
           <button className="flex gap-3 text-start max-h-15.5 cursor-pointer">
              <img
-                className="w-14 h-16 bg-black rounded-md object-cover "
+                className="min-w-14 h-16 bg-black rounded-md object-cover "
                 src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
                 alt=""
              />
